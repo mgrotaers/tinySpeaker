a19fe53fe3d21ac532107b11eafa1430a83409fa
mgrotaers <88262411+mgrotaers@users.noreply.github.com>
2022-05-18 12:53:18 +1000

    Update main.c

--- main.c
+++ main.c
@@ -3,26 +3,28 @@
 
 static void clock_setup(void)
 {
-  CLKPR = 0x10000000;
-  CLKPR = 0x00000000;
+	CLKPR = 0x10000000;
+	CLKPR = 0x00000000;
+}
 
 main (void)
 {
+	/*Setup Clock*/
+	cli;
+	clock_setup();
   
-  /*Setup Clock*/
-  cli;
-  clock_setup();
+	/*Setup Inputs and Outputs*/
+	DDRB &= _BV(DDB2); /*ADC1 Input*/
+	DDRB |= _BV(DDB4); /*PB4 PWM Output*/
   
-  /*Setup Inputs and Outputs*/
-  DDRB &= _BV(DDB2); //ADC1 Input
-  DDRB |= _BV(DDB4); //PB4 PWM Output
-  
-  
-  
+	/*Setup PWM*/
+	PLLCSR = _BV(PCKE) | _BV(PLLE);
+	GTCCR = _BV(PWM1B) | _BV(COM1B0);
+	TCCR1 = _BV(CS10);
 	
-	while(1)
+	
+	
+	while (1)
 	{
-		PORTB ^= _BV(PB0);
-		_delay_ms(3000);
 	}
 }
